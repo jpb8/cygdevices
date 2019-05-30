2d543591105528c41aad482d29633e45054c60db
Joseph.Ballard <joseph.ballard@tiga.us>
2019-05-30 16:36:18 -0500

    Error logging

--- device.py
+++ device.py
@@ -29,41 +29,44 @@ class DeviceDef:
     def device_dg_mappings(self, device_id, array_type):
         dgs = self.device_dgs_element(device_id)
         if dgs is None:
-            return False
+            return None, "Device {} Not Found".format(device_id)
         if dgs.find('./DataGroup/DataGroupAttributes/[DataGroupType="{}"].../UdcMappings'.format(array_type)) is None:
-            dg = DataGroup(device_id, device_id, array_type)
-            dgs.append(dg.dg_element)
-            print("appended {} {} DataGroup".format(device_id, array_type))
-        return dgs.find('./DataGroup/DataGroupAttributes/[DataGroupType="{}"].../UdcMappings'.format(array_type))
+            return None, "Array {} not found in Device {}".format(device_id, array_type)
+            # dg = DataGroup(device_id, device_id, array_type)
+            # dgs.append(dg.dg_element)
+            # print("appended {} {} DataGroup".format(device_id, array_type))
+        return dgs.find('./DataGroup/DataGroupAttributes/[DataGroupType="{}"].../UdcMappings'.format(array_type)), ""
 
     def add_maps(self, device_id, array_type, maps):
-        mappings = self.device_dg_mappings(device_id, array_type)
-        if mappings is not None:
-            for m in maps:
-                if mappings.find(".//UdcMapping[@UDC='{}'][@data_element_id='{}'][@facility='{}']".format(
-                        m.udc,
-                        m.data_id,
-                        m.fac)
-                ) is None:
-                    SubElement(mappings, "UdcMapping", {
-                        "UDC": m.udc,
-                        "data_element_id": m.data_id,
-                        "facility": m.fac
-                    })
-                else:
-                    print("{} {} {} mapping already exists".format(m.udc, m.data_id, m.fac))
-        else:
-            return False
+        mappings, err = self.device_dg_mappings(device_id, array_type)
+        errs = []
+        if mappings is None:
+            errs.append(err)
+            return errs
+        for m in maps:
+            if mappings.find(".//UdcMapping[@UDC='{}'][@data_element_id='{}'][@facility='{}']".format(
+                    m.udc,
+                    m.data_id,
+                    m.fac)
+            ) is None:
+                SubElement(mappings, "UdcMapping", {
+                    "UDC": m.udc,
+                    "data_element_id": m.data_id,
+                    "facility": m.fac
+                })
+            else:
+                errs.append("{} {} {} mapping already exists".format(m.udc, m.data_id, m.fac))
+        return errs
 
     def add_facs(self, facs, device_id):
         fac_elem = self.device_facs_element(device_id)
+        log = []
         if fac_elem is not None:
             for f in facs:
                 if fac_elem.find("./FacilityLink[@id='{}']".format(f)) is None:
                     SubElement(fac_elem, "FacilityLink", {"id": f, "ordinal": str(len(fac_elem))})
-                    print("{} was linkded".format(f))
-        else:
-            return False
+                    log.append("Facility {} was linkded to {} device".format(f, device_id))
+        return log
 
     def save(self, xml_file):
         et = ElementTree(self.xml)
--- test.py
+++ test.py
@@ -2,25 +2,40 @@ from device import DeviceDef, UdcMap
 from dtf import DTF
 import pandas as pd
 
-mappings = pd.read_excel("docs/Book2.xlsx", sheet_name="Sheet1")
-dd = DeviceDef("docs/dds_C15_MHR6_to_MID2.xml")
-dtfxml = DTF("docs/NGL_ET_AB_CIP_20190502-1318.dtf")
+mappings = pd.read_excel("docs/C15_delta0530.xlsx", sheet_name="Sheet1")
+dd = DeviceDef("docs/deviceDefinitions_20190530_pre_delta.xml")
+dtfxml = DTF("docs/NGL_DTF.dtf")
 
 
 def create_udc(dtfxml, row):
     _udc = None
+    err = None
     if row["type"] == "A":
         deid = dtfxml.get_analog_deid(da, row["indexed"], str(int(row["bit"])))
         if deid:
             _udc = UdcMap(row["uniformdatacode"], deid, row["facilityid"])
+        else:
+            err = "*DEID Not found* tagname: {}[{}], Array: {}, UDC: {} FAC: {}".format(
+                row["indexed"], row["bit"], da,
+                row["uniformdatacode"],
+                row["facilityid"]
+            )
     elif row["type"] == "D":
         deid_check = dtfxml.check_dg_element(row["array"], row["deid"])
         if deid_check:
             _udc = UdcMap(row["uniformdatacode"], row["deid"], row["facilityid"])
-    return _udc
+        else:
+            err = "*DEID Not found* DEID: {}, Array {}, UDC: {} FAC: {}".format(
+                row["deid"],
+                da,
+                row["uniformdatacode"],
+                row["facilityid"]
+            )
+    return _udc, err
 
 
 devices = mappings.device.unique()
+errs = []
 for d in devices:
     dev_points = mappings.loc[mappings['device'] == d]
     dev_arr = dev_points.array.unique()
@@ -30,13 +45,16 @@ for d in devices:
             arr_points = dev_points.loc[dev_points['array'] == da]
             maps = []
             for i, p in arr_points.iterrows():
-                udc = create_udc(dtfxml, p)
-                maps.append(udc) if udc is not None else print(
-                    "{} Not in {} Array".format(p["deid"], p["array"]))
+                udc, pnt_err = create_udc(dtfxml, p)
+                maps.append(udc) if udc is not None else errs.append(pnt_err)
                 facs.append(p["facilityid"]) if p["facilityid"] not in facs else None
-            dd.add_maps(d, da, maps)
-            dd.add_facs(facs, d)
+            map_log = dd.add_maps(d, da, maps)
+            fac_log = dd.add_facs(facs, d)
+            errs = errs + map_log + fac_log
     else:
-        print("Device {} is not in XML".format(d))
+        errs.append("Device {} is not in XML".format(d))
 
-dd.save("docs/C15_updated.xml")
+dd.save("docs/deviceDefinitions_20190530_updated.xml")
+with open('docs/errorLog.txt', 'w') as f:
+    for e in errs:
+        f.write("{}\n".format(e))
